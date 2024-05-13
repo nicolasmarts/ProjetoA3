d9680040ef792a655c42e8310bdc594568b3af19
nicolasmarts <martinsnicolasouza@gmail.com>
2024-05-13 19:47:53 -0300

    Add files via upload

---
+++ ProjetoA3.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projetoa3;
+
+/**
+ *
+ * @author 1072419604
+ */
+public class ProjetoA3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
